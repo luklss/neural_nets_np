2292f22592cb33f4e6574a064a789d8d624d42a2
luklss <luklss@BLSDKLBECT480S.localdomain>
2019-07-20 23:04:45 +0200

    first full not working implementation

--- models.py
+++ models.py
@@ -12,17 +12,26 @@ class SimpleNet:
         self.w = np.random.randn()
         self.b = np.random.randn()
 
-    def fit(self,
-            x,
-            y,
-            epochs,
-            lr = 0.01):
+    def fit(self, x, y, epochs, lr = 0.01):
 
-        for epoch in enumerate(epochs):
-            print("epoch {} started".format(epoch))
 
-            y_hat = self.predict(x)
-            error = self.mean_square_error(y, y_hat)
+        for i in range(epochs):
+            print("epoch {} started".format(i))
+
+            output_deltas = np.zeros(len(x))
+
+            z = self.w * x + self.b
+            y_hat = sigmoid(z)
+            error = mean_squared_error(y, y_hat)
+            de_dy = mean_squared_error_derivative(y, y_hat)
+            dy_dz = sigmoid_derivative(z)
+            dz_dw = x
+
+            de_dw = de_dy * dy_dz * dz_dw
+            de_db = de_dy * dy_dz
+
+            self.w = self.w - lr * de_dw
+            self.b = self.b - lr * de_db
 
 
     def predict(self, x):
@@ -30,17 +39,21 @@ class SimpleNet:
 
 
 
-    def mean_square_error(y, y_hat):
-        assert len(y_hat) == len(y)
-        return ((y - y_hat) ^2) / len(x)
+def mean_squared_error(y, y_hat):
+    assert len(y_hat) == len(y)
+    return ((y - y_hat) ** 2) / len(y)
+
+
+def mean_squared_error_derivative(y, y_hat):
+    return y - y_hat
 
 
 
-    def sigmoid(x):
-        return 1 / (1 + np.exp(-x))
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-    def sigmoid_derivative(x):
-        return sigmoid(x) * (1 - sigmoid(x))
+def sigmoid_derivative(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
 
